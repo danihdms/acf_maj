aeecff43bb1f9c7a8766da769b167bf58041edf2
SofiaBmr <85236463+SofiaBmr@users.noreply.github.com>
2021-06-14 16:07:01 +0200

    Delete coDB.php

--- coDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-   $pdo = new PDO('sqlite:dbACF.db');
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-   die('Erreur : '.$e->getMessage());
-}
